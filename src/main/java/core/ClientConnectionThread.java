2f7d2951cb19eb021c564f334faab3b5a34822bf
nullpointer-01 <>
2025-08-10 14:19:38 +0530

    Read stream byte wise instead of String

--- src/main/java/core/ClientConnectionThread.java
+++ src/main/java/core/ClientConnectionThread.java
@@ -2,10 +2,15 @@ package core;
 
 import util.RespSerializer;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static util.RespConstants.CR;
+import static util.RespConstants.LF;
 
 public class ClientConnectionThread extends Thread {
     private static final Logger LOGGER = Logger.getLogger(ClientConnectionThread.class.getName());
@@ -18,24 +23,46 @@ public class ClientConnectionThread extends Thread {
 
     @Override
     public void run() {
-        try (OutputStream outputStream = client.getOutputStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+        try (OutputStream outputStream = client.getOutputStream();
+             BufferedInputStream bis = new BufferedInputStream(client.getInputStream())) {
 
             String PONG = "+PONG\r\n";
-            String line;
+            StringBuilder sb = new StringBuilder();
+            int data;
 
-            while ((line = reader.readLine()) != null) {
-                switch (line) {
-                    case "PING":
-                        outputStream.write(PONG.getBytes());
-                        break;
-                    case "ECHO":
-                        reader.readLine(); // Skip CRLF
-                        String arg = reader.readLine();
-                        String response = RespSerializer.asBulkString(arg);
+            while ((data = bis.read()) != -1) {
+                char c = (char) data;
+                String command = sb.toString();
 
-                        outputStream.write(response.getBytes());
-                        break;
+                if (c == CR && (char) bis.read() == LF) {
+                    switch (command) {
+                        case "PING":
+                            outputStream.write(PONG.getBytes());
+                            break;
+                        case "ECHO":
+                            bis.read(); // Skip $
+                            c = (char) bis.read();
+
+                            int len = c - '0';
+                            while ((c = (char) bis.read()) != CR) {
+                                len *= 10;
+                                len += (c - '0');
+                            }
+
+                            System.out.println("len -> " + len);
+                            bis.read(); // Skip LF
+
+                            String arg = new String(bis.readNBytes(len));
+                            String response = RespSerializer.asBulkString(arg);
+
+                            outputStream.write(response.getBytes());
+                            break;
+                    }
+                    sb.setLength(0);
+                } else {
+                    sb.append(c);
                 }
+
                 outputStream.flush();
             }
         } catch (IOException e) {
--- src/main/java/util/RespConstants.java
+++ src/main/java/util/RespConstants.java
@@ -1,9 +1,11 @@
 package util;
 
 public class RespConstants {
+    public static final Character CR = '\r';
+    public static final Character LF = '\n';
+
     public static final String CRLF = "\r\n";
     public static final String DOLLAR = "$";
-
     public static final String EMPTY_BULK_STRING = "$0\r\n\r\n";
 
     // Private constructor to prevent instantiations
