fa6d80d3e69d5f2c41820317f4628b11af5a95cb
nullpointer-01 <>
2025-08-12 22:47:30 +0530

    Add resp protocol constants

--- src/main/java/util/RespConstants.java
+++ src/main/java/util/RespConstants.java
@@ -1,12 +1,20 @@
 package util;
 
 public class RespConstants {
+    public static final Character ASTERISK = '*';
+    public static final Character DOLLAR = '$';
+
     public static final Character CR = '\r';
     public static final Character LF = '\n';
+    public static final Character NULL = '\u0000';
+
 
     public static final String CRLF = "\r\n";
-    public static final String DOLLAR = "$";
     public static final String EMPTY_BULK_STRING = "$0\r\n\r\n";
+
+    public static final String PONG_SIMPLE_STRING = "+PONG\r\n";
+    public static final String OK_SIMPLE_STRING = "+OK\r\n";
+    public static final String NULL_BULK_STRING = "$-1\r\n";
 
     // Private constructor to prevent instantiations
     private RespConstants() {}
