529e2489ca4e5731db7679704fc6c26cda85b343
nullpointer-01 <>
2025-08-12 22:51:03 +0530

    Refactoring - Handle requests in service layer

--- src/main/java/core/ClientConnectionThread.java
+++ src/main/java/core/ClientConnectionThread.java
@@ -1,16 +1,16 @@
 package core;
 
-import util.RespSerializer;
+import model.Request;
+import model.Response;
+import service.RequestHandler;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static util.RespConstants.CR;
-import static util.RespConstants.LF;
 
 public class ClientConnectionThread extends Thread {
     private static final Logger LOGGER = Logger.getLogger(ClientConnectionThread.class.getName());
@@ -26,47 +26,20 @@ public class ClientConnectionThread extends Thread {
         try (OutputStream outputStream = client.getOutputStream();
              BufferedInputStream bis = new BufferedInputStream(client.getInputStream())) {
 
-            String PONG = "+PONG\r\n";
-            StringBuilder sb = new StringBuilder();
-            int data;
+            while (client.isConnected()) {
+                if (bis.available() > 0) {
+                    List<Request> requests = RequestHandler.parseRequests(bis);
 
-            while ((data = bis.read()) != -1) {
-                char c = (char) data;
-                String command = sb.toString();
+                    for (Request request : requests) {
+                        Response response = request.execute();
 
-                if (c == CR && (char) bis.read() == LF) {
-                    switch (command) {
-                        case "PING":
-                            outputStream.write(PONG.getBytes());
-                            break;
-                        case "ECHO":
-                            bis.read(); // Skip $
-                            c = (char) bis.read();
-
-                            int len = c - '0';
-                            while ((c = (char) bis.read()) != CR) {
-                                len *= 10;
-                                len += (c - '0');
-                            }
-
-                            System.out.println("len -> " + len);
-                            bis.read(); // Skip LF
-
-                            String arg = new String(bis.readNBytes(len));
-                            String response = RespSerializer.asBulkString(arg);
-
-                            outputStream.write(response.getBytes());
-                            break;
+                        outputStream.write(response.getResponse());
+                        outputStream.flush();
                     }
-                    sb.setLength(0);
-                } else {
-                    sb.append(c);
                 }
-
-                outputStream.flush();
             }
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception processing client connection. " + e);
+            LOGGER.log(Level.SEVERE, "Exception processing client request. " + e);
         } finally {
             if (client != null) {
                 try {
---
+++ src/main/java/service/RequestHandler.java
@@ -0,0 +1,44 @@
+package service;
+
+import model.*;
+import util.RespDeserializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static util.RespConstants.ASTERISK;
+
+public class RequestHandler {
+    public static List<Request> parseRequests(InputStream is) throws IOException {
+        List<Request> requests = new ArrayList<>();
+        char c = (char) is.read();
+        if (c != ASTERISK) {
+            throw new IOException("Invalid byte, expected array " + c);
+        }
+
+        List<Object> itemsTmp = RespDeserializer.parseArray(is);
+        List<String > items = itemsTmp.stream().map(i -> (String) i).toList();
+        Command command = Command.getCommandByName(items.get(0));
+
+        switch (command) {
+            case PING:
+                requests.add(new PingRequest());
+                break;
+            case ECHO:
+                requests.add(new EchoRequest(items.get(1)));
+                break;
+            case SET:
+                requests.add(new SetRequest(items.get(1), items.get(2)));
+                break;
+            case GET:
+                requests.add(new GetRequest(items.get(1)));
+                break;
+            default:
+                throw new IOException("Invalid command");
+        }
+
+        return requests;
+    }
+}
