3fc0cdfef837041cc7bb2275750cda3a81b985a2
nullpointer-01 <>
2025-08-14 12:20:49 +0530

    Add util for Resp array serialization

--- src/main/java/util/RespSerializer.java
+++ src/main/java/util/RespSerializer.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.List;
+
 import static util.RespConstants.*;
 
 public class RespSerializer {
@@ -16,5 +18,15 @@ public class RespSerializer {
     public static String asInteger(Integer value) {
         String sign = value < 0 ? "-" : "";
         return COLON + sign + value + CRLF;
+    }
+
+    public static String asArray(List<String> elements) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ASTERISK).append(elements.size()).append(CRLF);
+
+        for (String ele : elements) {
+            sb.append(asBulkString(ele));
+        }
+        return sb.toString();
     }
 }
